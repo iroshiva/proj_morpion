4070e42d2e1437ba71455b1e5c8441c6a7d941ef
iroshiva <jean.weil@protonmail.com>
2019-07-24 22:27:24 +0200

    début class Game : initialize, player1_turn, player2_turn

--- lib/app/game.rb
+++ lib/app/game.rb
@@ -2,15 +2,27 @@
 # require_relative './board'
 
 class Game
+# c'est le jeu. Elle initialise tout, lance une partie (qui se termine avec une victoire ou un nul), permet de jouer un tour, de chercher si la partie est finie, etc.
+# prend une instance
 
-
+	attr_accessor :players
+	# l'instance créée prends les variables players
 	
 
-	attr_accessor 
-	# chaque instance aura deux variables : som nom et un symbole
+	def initialize(name, symbole)
+		@players = [player1 = Player.new(name1, "X"), player2 = Player.new(name2, "Y")]
+	end
 
-	def initialize
+	def player1_turn
+		puts "Your turn #{@players[0]}!"
+	end
 
+	def player1_turn
+		puts "Your turn #{@players[0]}!"
+	end
+
+	def case
+		puts "Pick up a free case!!!!"
 	end
 
 
