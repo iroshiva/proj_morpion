f1cd8e869d392c46107e7497d4826d478757b72a
iroshiva <jean.weil@protonmail.com>
2019-07-24 18:35:42 +0200

    class + def initialize pour la class Player

--- app.rb
+++ app.rb
@@ -3,3 +3,7 @@ Bundler.require
 
 $:.unshift File.expand_path("./../lib", __FILE__)
 require 'app/player'
+
+
+# teste = App.new
+
---
+++ lib/app/player.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+
+class Player
+
+	attr_accessor :name, :symbol
+	# chaque instance aura deux variables : som nom et un symbole
+
+	def initialize(name, symbol)
+		@name = name 
+		@symbol = symbol
+	end
+
+end
+
+binding.pry
+puts "end of file"
