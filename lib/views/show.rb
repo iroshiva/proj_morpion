cc14cd82fdbabedec9ef90acb7db532fa97c77e5
iroshiva <jean.weil@protonmail.com>
2019-07-26 00:12:33 +0200

    last.... pb de chiffre...

--- lib/views/show.rb
+++ lib/views/show.rb
@@ -9,7 +9,7 @@ class Show
     rows_morp = [
       [ @game.board.board_array[0].symbols, @game.board.board_array[1].symbols, @game.board.board_array[2].symbols],
       [ @game.board.board_array[3].symbols, @game.board.board_array[4].symbols, @game.board.board_array[5].symbols],
-      [ @game.board.board_array[6].symbols, @game.board.board_array[8].symbols, @game.board.board_array[8].symbols] 
+      [ @game.board.board_array[6].symbols, @game.board.board_array[7].symbols, @game.board.board_array[8].symbols] 
     ]
     Terminal::Table.new :headings => ['1','2','3'],:rows => rows_morp
   end
