0873a7ef712c2c8f79fa4f9c21727d484084b352
iroshiva <jean.weil@protonmail.com>
2019-07-25 09:25:26 +0200

    ajout boardcase.rb

---
+++ lib/app/boardcase.rb
@@ -0,0 +1,12 @@
+
+
+class Boardcase
+# c'est une case. Il devrait y avoir 9 instances de cette classe lors d'une partie.
+# créer 9 instances == 9 objets case qui constitueront les 9 cases à insérer dans la class Board
+	
+	
+
+
+
+
+end
